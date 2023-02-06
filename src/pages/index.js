57cca560842a45d864b46806d34c5ffb157ab567
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-06 17:41:19 +0100

    add components home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import MainLayout from 'components/layout/Main-layout'
+import AboutMe from 'components/sections/AboutMe'
 import HomeSection from 'components/sections/Home'
+import PortfolioHome from 'components/sections/PortfolioHome'
 
 export default function Home() {
   return (
     <>
       <MainLayout language='esp'>
         <HomeSection />
+        <PortfolioHome />
+        <AboutMe />
       </MainLayout>
     </>
   )
