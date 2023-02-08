e475ce2487693d76d289af672cbaefc6dfdd4440
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-08 18:08:49 +0100

    fix home component

--- components/sections/Home.jsx
+++ components/sections/Home.jsx
@@ -1,5 +1,4 @@
 import SocialNetworks from 'components/ui/SocialNetworks'
-import Image from 'next/image'
 import React from 'react'
 import style from 'src/styles/Home.module.css'
 
@@ -8,24 +7,18 @@ export const HomeSection = () => {
     <main className={style.homeBackground}>
       <section className={style.homeContainer}>
         <picture className={style.HomePicture}>
-          {/* <img src='/img_static/carlosFoto.webp' /> */}
-          <Image
-            src={'/img_static/carlosFoto.webp'}
-            width={700}
-            height={700}
-            // fill
-            alt='Fotografia de Carlos Ajenjo'
-          />
+          <img className={style.photo} src='/img_static/carlosFoto.webp' />
         </picture>
-        <h1 className={style.homeLogo}>
-          Carlos Ajenjo <br /> <span>Web Developer</span>
-        </h1>
 
-        <section className={style.homeSocialNetworksGroup}>
-          {<SocialNetworks color='#ededed' />}
-        </section>
-
-        <button className={style.btnContact}>Contacto</button>
+        <article className={style.info}>
+          <h1 className={style.homeLogo}>
+            Carlos Ajenjo <br /> <span>Web Developer</span>
+          </h1>
+          <section className={style.homeSocialNetworksGroup}>
+            {<SocialNetworks color='#ededed' />}
+          </section>
+          <button className={style.btnContact}>Contacto</button>
+        </article>
       </section>
     </main>
   )
