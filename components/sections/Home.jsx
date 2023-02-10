53b15ac3b15213354b38360c1a970e843ea37caf
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-10 13:51:12 +0100

    add. component Image

--- components/sections/Home.jsx
+++ components/sections/Home.jsx
@@ -1,24 +1,33 @@
 import SocialNetworks from 'components/ui/SocialNetworks'
+import Image from 'next/image'
 import React from 'react'
 import style from 'src/styles/Home.module.css'
+import photoCarlos from '../../public/img_static/carlosFoto.webp'
 
 export const HomeSection = () => {
   return (
     <main className={style.homeBackground}>
       <section className={style.homeContainer}>
-        <picture className={style.HomePicture}>
-          <img className={style.photo} src='/img_static/carlosFoto.webp' />
-        </picture>
-
         <article className={style.info}>
           <h1 className={style.homeLogo}>
             Carlos Ajenjo <br /> <span>Web Developer</span>
           </h1>
+
           <section className={style.homeSocialNetworksGroup}>
             {<SocialNetworks color='#ededed' />}
           </section>
+
           <button className={style.btnContact}>Contacto</button>
         </article>
+
+        <picture className={style.HomePicture}>
+          <Image
+            className={style.photo}
+            src={photoCarlos}
+            alt='foto de carlos Ajenjo'
+            fill
+          />
+        </picture>
       </section>
     </main>
   )
