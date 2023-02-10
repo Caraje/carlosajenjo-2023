d5e245b05735e6911b5bdcbc780c7b24321d694e
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-10 17:14:10 +0100

    create: index for portfolioPage

---
+++ src/pages/portfolio/index.js
@@ -0,0 +1,11 @@
+import MainLayout from 'components/layout/Main-layout'
+
+export default function PortfolioPage({ isMobile }) {
+  return (
+    <>
+      <MainLayout isMobile={isMobile} language='esp'>
+        Aqui van la coleccion completa de trabajos para el portfolio
+      </MainLayout>
+    </>
+  )
+}
