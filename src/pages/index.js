11b30ad557d62f80cfe743b34bb50cb375b64692
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-10 17:15:09 +0100

    delete: check width of screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,12 @@ import MainLayout from 'components/layout/Main-layout'
 import AboutMe from 'components/sections/AboutMe'
 import HomeSection from 'components/sections/Home'
 import PortfolioHome from 'components/sections/PortfolioHome'
-import useWindowDimensions from 'hooks/useWindowDimension'
-import { useEffect, useState } from 'react'
+
 
 export default function Home() {
-  const [isMobile, setIsMobile] = useState(false)
-  const { width, height } = useWindowDimensions()
-  useEffect(() => {
-    width < 764 ? setIsMobile(true) : setIsMobile(false)
-  }, [width])
-
   return (
     <>
-      <MainLayout isMobile={isMobile} language='esp'>
+      <MainLayout language='esp'>
         <HomeSection />
         <PortfolioHome />
         <AboutMe />
