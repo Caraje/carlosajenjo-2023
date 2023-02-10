227f62eabb28d14c124aa4f663930111e710fa02
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-10 13:52:02 +0100

    add: Anchor in links

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import style from 'src/styles/Navbar.module.css'
 import Menu from './Menu'
 import menuIcon from '../../public/img_static/icons/menu_icon.svg'
+import logo from '../../public/img_static/logos/logo-dark.svg'
 
 const Navbar = ({ theme, lang, isMobile }) => {
   const [isOpen, setIsOpen] = useState(false)
@@ -10,23 +11,30 @@ const Navbar = ({ theme, lang, isMobile }) => {
   return (
     <>
       <div className={style.navbar}>
-        <Image
-          src='/img_static/logos/logo-dark.svg'
-          alt='Logo Carlosajenjo.es'
-          width={200}
-          height={100}
-        />
-        {isMobile ? (
-          <button
-            onClick={() => {
-              setIsOpen(!isOpen)
-            }}
-          >
-            <Image src={menuIcon} alt='Icono de despliegue del menu' />
-          </button>
-        ) : (
-          <Menu theme={theme} lang={lang} isMobile={false} />
-        )}
+        <div className={style.containerNavbar}>
+          <a href='/' className={style.logoContainer}>
+            <Image
+              className={style.logoimg}
+              src={logo}
+              alt='Logo Carlosajenjo.es'
+              fill
+            />
+          </a>
+          {isMobile ? (
+            <button
+              onClick={() => {
+                setIsOpen(!isOpen)
+              }}
+              className={style.btnMenuMobile}
+            >
+              <Image src={menuIcon} alt='Icono de despliegue del menu' fill />
+            </button>
+          ) : (
+            <section className={style.menuContainer}>
+              <Menu theme={theme} lang={lang} isMobile={false} />
+            </section>
+          )}
+        </div>
       </div>
       {isOpen && <Menu theme={theme} lang={lang} isMobile={true} />}
     </>
