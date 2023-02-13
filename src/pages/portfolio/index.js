3a77971b87efd214ce3c814709f77e46fd1f41a0
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-13 17:24:52 +0100

    add: index portfolio page

--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,11 +1,23 @@
 import MainLayout from 'components/layout/Main-layout'
+import PortfolioCard from 'components/ui/PortfolioCard'
+import { db } from 'data/infoBase'
+import style from 'src/styles/PortfolioPage.module.css'
 
 export default function PortfolioPage({ isMobile }) {
   return (
-    <>
+    <div className={style.portfolioBackground}>
       <MainLayout isMobile={isMobile} language='esp'>
-        Aqui van la coleccion completa de trabajos para el portfolio
+        <main className={style.portfolioContainer}>
+          <h1 className={style.portfolioTitle}>Portfolio</h1>
+          <section className={style.portfolioGrid}>
+            {db.portfolio
+              .sort((a, b) => a.id - b.id)
+              .map((card) => (
+                <PortfolioCard key={card.id} card={card} />
+              ))}
+          </section>
+        </main>
       </MainLayout>
-    </>
+    </div>
   )
 }
