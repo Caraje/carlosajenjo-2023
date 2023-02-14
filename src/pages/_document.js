8cedaa9fa2a8fbdd6e06c8d3e887b37271a91674
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-02-14 18:13:17 +0100

    fix:color theme

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang='es'>
+    <Html lang='es' data-theme={false}>
       <Head>
         <link rel='shortcut icon' href='/favicon.ico' />
       </Head>
@@ -12,5 +12,5 @@ export default function Document() {
       </body>
       <footer></footer>
     </Html>
-  );
+  )
 }
