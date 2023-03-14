429629463fde9faffb113b71ef0d87744b4a46ba
Carlos Ajenjo <carlosajenjo@gmail.com>
2023-03-14 19:58:25 +0100

    fix: Anchor social Networks

--- components/ui/SocialNetworks.jsx
+++ components/ui/SocialNetworks.jsx
@@ -4,7 +4,7 @@ import { socialNetworks } from 'utils/networks'
 
 const SocialNetworks = ({ color }) => {
   return socialNetworks.map((soc) => (
-    <a key={soc.name} href={soc.url}>
+    <a key={soc.name} href={soc.url} target='_blank' rel='noreferrer'>
       {<Icons icon={soc.icon} color={color} />}
     </a>
   ))
